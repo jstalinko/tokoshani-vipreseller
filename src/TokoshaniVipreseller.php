eb589ae4d7a0f8a2512930922d6f496cd396777f
jstalinko <alinkodotid@gmail.com>
2024-08-04 18:04:49 +0700

    get services: game,prepaid

--- src/TokoshaniVipreseller.php
+++ src/TokoshaniVipreseller.php
@@ -12,17 +12,17 @@ class TokoshaniVipreseller
         $this->client = new Client(['base_uri' => 'https://vip-reseller.co.id/api/']);
     }
 
-    public function generateSign()
+    public function generateSign(): string
     {
         $apiId = config('tokoshani-vipreseller.api_id');
         $apiKey = config('tokoshani-vipreseller.api_key');
         return md5($apiId . $apiKey);
     }
 
-    public function getProfile()
+    public function getProfile(): string
     {
 
-        
+
         $response = $this->client->request('POST', 'profile', [
             'form_params' => [
                 'key' => config('tokoshani-vipreseller.api_key'),
@@ -34,15 +34,63 @@ class TokoshaniVipreseller
         return $response->getBody()->getContents();
     }
 
-    public function getPrepaid()
-    {
-        // $response = $this->client->request('POST','prepaid',
-        // [
-        //     'forms_params' => [
-        //         'key' => config('tokoshani-vipreseller.api_key'),
-        //         'sign' => $this->generateSign(),
 
-        //     ]
-        // ])
+    public function getGameFeatureServices(?string $filter_type = null, ?string $filter_value = null, ?string $filter_status = null): string
+    {
+        // Base form parameters
+        $formParams = [
+            'key' => config('tokoshani-vipreseller.api_key'),
+            'sign' => $this->generateSign(),
+            'type' => 'service',
+        ];
+
+        // Add optional filter parameters if they are provided
+        if ($filter_type !== null) {
+            $formParams['filter_type'] = $filter_type;
+        }
+
+        if ($filter_value !== null) {
+            $formParams['filter_value'] = $filter_value;
+        }
+
+        if ($filter_status !== null) {
+            $formParams['filter_status'] = $filter_status;
+        }
+
+        // Make the HTTP request
+        $response = $this->client->request('POST', 'game-feature', [
+            'form_params' => $formParams,
+        ]);
+
+        return $response->getBody()->getContents();
     }
+
+    /** 
+    * @method getPrepaidServices
+    * @var $filter_type  type | brand
+    * @var $filter_value 
+    */
+    public function getPrepaidServices(?string $filter_type = null , ?string $filter_value = null): string
+    {
+         // Base form parameters
+         $formParams = [
+            'key' => config('tokoshani-vipreseller.api_key'),
+            'sign' => $this->generateSign(),
+            'type' => 'service',
+        ];
+        if ($filter_type !== null) {
+            $formParams['filter_type'] = $filter_type;
+        }
+
+        if ($filter_value !== null) {
+            $formParams['filter_value'] = $filter_value;
+        }
+        // Make the HTTP request
+        $response = $this->client->request('POST', 'prepaid', [
+            'form_params' => $formParams,
+        ]);
+
+        return $response->getBody()->getContents();
+    }
+
 }
--- tests/VipTest.php
+++ tests/VipTest.php
@@ -16,6 +16,8 @@ use Jstalinko\TokoshaniVipreseller\TokoshaniVipresellerFacade as Vipreseller;
 class VipTest extends TestCase
 {
     use CreatesApplication;
+    public $apiKey;
+    public $sign;
 
     protected function getPackageProviders($app)
     {
@@ -26,6 +28,8 @@ class VipTest extends TestCase
     {
         parent::setUp();
         $this->app->register(TokoshaniVipresellerServiceProvider::class);
+        $this->apiKey = env('TOKOSHANI_VIPRESELLER_API_KEY');
+        $this->sign = env('TOKOSHANI_VIPRESELLER_SIGN');
     }
 
     public function test_sign()
@@ -37,8 +41,8 @@ class VipTest extends TestCase
 
     public function test_getprofile_success()
     {
-        $apiKey = env('TOKOSHANI_VIPRESELLER_API_KEY');
-        $sign = env('TOKOSHANI_VIPRESELLER_SIGN');
+        $apiKey = $this->apiKey;
+        $sign = $this->sign;
 
         // Create a mock response
         $mockResponseBody = file_get_contents(__DIR__ . '/mock/profile.json');
@@ -60,6 +64,59 @@ class VipTest extends TestCase
         $response = $tokoshaniVipreseller->getProfile();
         $responseBody = json_decode($response, true);
 
+        $this->assertTrue($responseBody['result']);
+    }
+
+    public function test_get_game_services()
+    {
+        $apiKey = $this->apiKey;
+        $sign = $this->sign;
+
+        // Create a mock response
+        $mockResponseBody = file_get_contents(__DIR__ . '/mock/gameFeatureServices.json');
+        $mockResponse = new Response(200, [], $mockResponseBody);
+
+        // Create a mock handler and stack
+        $mockHandler = new MockHandler([$mockResponse]);
+        $handlerStack = HandlerStack::create($mockHandler);
+        $client = new Client(['handler' => $handlerStack]);
+
+        // Create an instance of TokoshaniVipreseller with the mocked client
+        $tokoshaniVipreseller = new TokoshaniVipreseller($client);
+
+        // Mock the config and generateSign method
+        $this->app['config']->set('tokoshani-vipreseller.api_key', $apiKey);
+        Vipreseller::shouldReceive('generateSign')->andReturn($sign);
+
+        // Call the getProfile method and assert the response
+        $response = $tokoshaniVipreseller->getGameFeatureServices();
+        $responseBody = json_decode($response, true);
+        $this->assertTrue($responseBody['result']);
+    }
+    public function test_get_prepaid_services()
+    {
+        $apiKey = $this->apiKey;
+        $sign = $this->sign;
+
+        // Create a mock response
+        $mockResponseBody = file_get_contents(__DIR__ . '/mock/prepaidServices.json');
+        $mockResponse = new Response(200, [], $mockResponseBody);
+
+        // Create a mock handler and stack
+        $mockHandler = new MockHandler([$mockResponse]);
+        $handlerStack = HandlerStack::create($mockHandler);
+        $client = new Client(['handler' => $handlerStack]);
+
+        // Create an instance of TokoshaniVipreseller with the mocked client
+        $tokoshaniVipreseller = new TokoshaniVipreseller($client);
+
+        // Mock the config and generateSign method
+        $this->app['config']->set('tokoshani-vipreseller.api_key', $apiKey);
+        Vipreseller::shouldReceive('generateSign')->andReturn($sign);
+
+        // Call the getProfile method and assert the response
+        $response = $tokoshaniVipreseller->getPrepaidServices();
+        $responseBody = json_decode($response, true);
         $this->assertTrue($responseBody['result']);
     }
 }
