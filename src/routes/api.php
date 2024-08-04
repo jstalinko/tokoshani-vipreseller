42fddf7cb181f5d7f50b4361bfd9a7f257a5c478
jstalinko <alinkodotid@gmail.com>
2024-08-04 20:38:13 +0700

    fix typo

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,10 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use Jstalinko\TokoshaniVipreseller\Http\Controllers\VipresellerController;
 
-Route::group(['prefix' => 'shn-api', function () {
+Route::group(['prefix' => 'shn-api'], function () {
     Route::get('/profile', [VipresellerController::class, 'getProfile'])->name('tokoshani.vipreseller.profile');
     Route::group(['prefix' => '/services'], function () {
         Route::get('/games', [VipresellerController::class, 'getGameFeatureServices'])->name('tokoshani.vipreseller.gamefeatureservices');
         Route::get('/prepaid', [VipresellerController::class, 'getPrepaidServices'])->name('tokoshani.vipreseller.prepaidservices');
     });
-}]);
+});
